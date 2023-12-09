0bc6e3539343281a592fff024cd56e182850038f
jae0eaj <86229084+jae0eaj@users.noreply.github.com>
2023-12-10 04:27:59 +0900

    Update index.js

--- index.js
+++ index.js
@@ -60,30 +60,30 @@ function mobile(e) {
 
 
 
-/* 모바일 세로스크롤 X */ 
-function setScreenSize() {
-	let vh = window.innerHeight * 0.01;
-	document.documentElement.style.setProperty('--vh', `${vh}px`);
-}
-setScreenSize();
-window.addEventListener('resize', setScreenSize);
-
-
-
-// /* 스크롤 이벤트 자체 제거*/ 
-// disableScroll = () => {
-//     document.querySelector('body').addEventListener('touchmove', this.removeEvent, { passive: false });
-//     document.querySelector('body').addEventListener('onclick', this.removeEvent, { passive: false });
-//     document.querySelector('body').addEventListener('mousewheel', this.removeEvent, { passive: false });
-//   }
-
-//   removeEvent = e => {
-//     e.preventDefault();
-//     e.stopPropagation();
-//   }
-
-//   enableScroll = () => {
-//     document.querySelector('body').removeEventListener('touchmove', this.removeEvent);
-//     document.querySelector('body').removeEventListener('onclick', this.removeEvent);
-//     document.querySelector('body').removeEventListener('mousewheel', this.removeEvent);
+// /* 모바일 세로스크롤 X */ 
+// function setScreenSize() {
+// 	let vh = window.innerHeight * 0.01;
+// 	document.documentElement.style.setProperty('--vh', `${vh}px`);
 // }
+// setScreenSize();
+// window.addEventListener('resize', setScreenSize);
+
+
+
+/* 스크롤 이벤트 자체 제거*/ 
+disableScroll = () => {
+    document.querySelector('body').addEventListener('touchmove', this.removeEvent, { passive: false });
+    document.querySelector('body').addEventListener('onclick', this.removeEvent, { passive: false });
+    document.querySelector('body').addEventListener('mousewheel', this.removeEvent, { passive: false });
+  }
+
+  removeEvent = e => {
+    e.preventDefault();
+    e.stopPropagation();
+  }
+
+  enableScroll = () => {
+    document.querySelector('body').removeEventListener('touchmove', this.removeEvent);
+    document.querySelector('body').removeEventListener('onclick', this.removeEvent);
+    document.querySelector('body').removeEventListener('mousewheel', this.removeEvent);
+}
